8f56c534dec6da962a90153a66e2c49eaaefb225
Ayush Chugh <ayushchugh2006@gmail.com>
2023-08-24 14:05:06 +0530

    feat: blury modal

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { SafeAreaView, View, Modal as ModalComponent } from "react-native";
 import { Heading } from "../typography/Typography";
+import { BlurView } from "expo-blur";
 
 interface ModalProps {
 	/**
@@ -23,14 +24,23 @@ interface ModalProps {
 
 const Modal: FC<ModalProps> = ({ isVisible, setIsVisible, heading, children }) => {
 	return (
-		<ModalComponent visible={isVisible} onRequestClose={() => setIsVisible(false)} transparent>
-			<SafeAreaView className="bg-grey/40" />
-			<View className="flex flex-1 h-full items-center justify-center bg-grey/40">
-				<View className="bg-white p-5 rounded w-[90%]">
+		<ModalComponent
+			visible={isVisible}
+			onRequestClose={() => setIsVisible(false)}
+			transparent
+			animationType="slide"
+			className="backdrop-blur-3xl"
+		>
+			<SafeAreaView />
+			<BlurView
+				intensity={100}
+				className="flex flex-1/2 h-[70%] absolute bottom-0 left-0 right-0 items-center justify-center bg-white/20 rounded-2xl backdrop-blur-1xl"
+			>
+				<View className="p-5 rounded w-[90%]">
 					<Heading rootClassName="mb-5">{heading}</Heading>
 					<View>{children}</View>
 				</View>
-			</View>
+			</BlurView>
 		</ModalComponent>
 	);
 };
